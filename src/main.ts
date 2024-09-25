ded2bccff35a3ddf10576e54d5736f2e5ae661cc
이종수 <whdtnxd@gmail.com>
2024-09-25 16:30:04 +0900

    :package: 이종수: ui 컴포넌트들, 테스트 완료

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "primevue/resources/themes/arya-blue/theme.css"; // 원하는 테마 CSS
+import "primevue/resources/primevue.min.css"; // PrimeVue 기본 스타일
+import "primeicons/primeicons.css"; // PrimeIcons 스타일
 
 createApp(App).use(store).use(router).mount("#app");
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,17 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Button icon="pi pi-home" aria-label="Save" />
+    <Button label="Profile" icon="pi pi-user" />
+    <Button label="Save" icon="pi pi-check" iconPos="right" />
+    <Button label="Search" icon="pi pi-search" iconPos="top" />
+    <Button label="Update" icon="pi pi-refresh" iconPos="bottom" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Button from "primevue/button";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     HelloWorld,
+    Button,
   },
 });
 </script>
