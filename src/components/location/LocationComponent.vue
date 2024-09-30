1682e9b515a9c6bf26c118faa5d631d4ab9578b3
최유진 <yjyj0774@gmail.com>
2024-09-30 15:29:22 +0900

    :wrench: 최유진 : location inputter 컴포넌트 생성

--- src/components/location/LocationComponent.vue
+++ src/components/location/LocationComponent.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <h1>Location</h1>
+    <LocationInputter />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import LocationInputter from "./input/LocationInputter.vue";
 
-export default defineComponent({});
+export default defineComponent({
+  name: "LocationComponent",
+  components: {
+    LocationInputter,
+  },
+});
 </script>
