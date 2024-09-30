1e79ce0d3c0b84b70bbd1ced680cc3145103e7b1
최유진 <yjyj0774@gmail.com>
2024-09-30 15:29:53 +0900

    :wrench: 최유진 : map displayer 컴포넌트 생성

--- src/components/location/LocationComponent.vue
+++ src/components/location/LocationComponent.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <h1>Location</h1>
+    <MapDisplayer />
     <LocationInputter />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import MapDisplayer from "./map/MapDisplayer.vue";
 import LocationInputter from "./input/LocationInputter.vue";
 
 export default defineComponent({
   name: "LocationComponent",
   components: {
+    MapDisplayer,
     LocationInputter,
   },
 });
